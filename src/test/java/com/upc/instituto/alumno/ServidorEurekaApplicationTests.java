658f851f24540b7c889820d121fbddc18f4ef5cc
Luis Guisado <luis.guisado.matrix@gmail.com>
2023-02-20 00:26:20 -0500

    First commit

---
+++ src/test/java/com/upc/instituto/alumno/ServidorEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.instituto.alumno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
